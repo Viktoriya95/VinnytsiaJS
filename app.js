169c17ba18b420fb54047432a40b34bdc26d7365
Viktoriya95 <gvm95@ukr.net>
2018-05-24 18:05:44 +0300

    Add files via upload

--- app.js
+++ app.js
@@ -4,11 +4,20 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+const mongoose = require('mongoose');
+mongoose.connect("mongodb+srv://User3:test@brainbasketcheckin-h7hkk.mongodb.net/checkin");
+const User = require('./Models/userSchema.js');
+const db = mongoose.connection;
+const userModel = db.model('test_users', User);
+var jwt = require('jsonwebtoken'); // used to create, sign, and verify tokens
+var config = require('./config.json'); // get our config file
+var token;
+
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var secretRouter = require('./routes/secret');
 var authenticateRouter = require('./routes/authenticate');
-var registerRouter = require('./routes/register');
+//var registerRouter = require('./routes/register');
 
 var app = express();
 
@@ -26,11 +35,42 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/secret', secretRouter);
 app.use('/authenticate', authenticateRouter);
-app.use('/register', registerRouter);
+//app.use('/register', registerRouter);
 
 /*app.use('/', function(req, res) {
   res.render('index');
 });*/
+
+app.post('/authenticate', function(req, res){
+      // find the user
+    userModel.findOne({name: req.body.name}, function(err, user) {
+    if (err) throw err;
+    if (!user) {
+      res.json({ success: false, message: 'Authentication failed. User not found.' });
+    } else if (user) {
+      // check if password matches
+      if (user.password != req.body.password) {
+        res.json({ success: false, message: 'Authentication failed. Wrong password.' });
+      } else {
+        // if user is found and password is right
+        // create a token with only our given payload
+    // we don't want to pass in the entire user since that has the password
+    const payload = {
+      admin: user.admin
+    };
+        token = jwt.sign(payload, config.secret, {
+          expiresInMinutes: 1440 // expires in 24 hours
+        });
+        // return the information including token as JSON
+        res.json({
+          success: true,
+          message: 'Enjoy your token!',
+          token: token
+        });
+      }
+    }
+  });
+})
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/authenticate.js
+++ routes/authenticate.js
@@ -1,45 +1,9 @@
 var express = require('express');
 var router = express.Router();
-const mongoose = require('mongoose');
-mongoose.connect("mongodb+srv://User3:test@brainbasketcheckin-h7hkk.mongodb.net/checkin");
-var jwt = require('jsonwebtoken'); // used to create, sign, and verify tokens
-var config = require('./config'); // get our config file
-var token;
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.render('auth');
+  res.render('authenticate');
 });
-
-router.post('/authenticate', function(req, res){
-      // find the user
-    userModel.findOne({name: req.body.name}, function(err, user) {
-    if (err) throw err;
-    if (!user) {
-      res.json({ success: false, message: 'Authentication failed. User not found.' });
-    } else if (user) {
-      // check if password matches
-      if (user.password != req.body.password) {
-        res.json({ success: false, message: 'Authentication failed. Wrong password.' });
-      } else {
-        // if user is found and password is right
-        // create a token with only our given payload
-    // we don't want to pass in the entire user since that has the password
-    const payload = {
-      admin: user.admin 
-    };
-        token = jwt.sign(payload, config.secret, {
-          expiresInMinutes: 1440 // expires in 24 hours
-        });
-        // return the information including token as JSON
-        res.json({
-          success: true,
-          message: 'Enjoy your token!',
-          token: token
-        });
-      }   
-    }
-  });
-})
 
 module.exports = router;
